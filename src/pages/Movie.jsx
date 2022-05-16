fe2c5c41f59a1fed0f8974f5f42944fa47799cb2
DamCod <98166110+DamCod@users.noreply.github.com>
2022-05-16 20:10:33 -0300

    arreglos en movie.jsx

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -5,31 +5,10 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { styled } from "@mui/material/styles";
 import Rating from "@mui/material/Rating";
-import Chip from "@mui/material/Chip";
-import Stack from "@mui/material/Stack";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { Modal } from "react-bootstrap";
 import ScrollToTopBtn from "../components/ScrollToTopBtn/ScrollToTopBtn";
-
-const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 6,
-    slidesToSlide: 1, // optional, default to 1.
-    partialVisibilityGutter: 20,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 3,
-    slidesToSlide: 1, // optional, default to 1.
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-    slidesToSlide: 1, // optional, default to 1.
-  },
-};
 
 function Movie() {
   const [movie, setMovie] = useState([]);
@@ -37,6 +16,7 @@ function Movie() {
   const [cast, setCast] = useState([]);
   const [director, setDirector] = useState([]);
   const [writer, setWriter] = useState([]);
+  const [reviews, setReviews] = useState([]);
 
   useEffect(() => {
     const getMovie = async () => {
@@ -45,9 +25,13 @@ function Movie() {
         `/movie/${params.id}/credits`,
         tmdbApiConfig
       );
+      const reviewsData = await axios.get(
+        `/movie/${params.id}/reviews`,
+        tmdbApiConfig
+      );
       setMovie(data);
-      console.log(data);
-      console.log(response.data.crew);
+      setReviews(reviewsData.data.results);
+      console.log(reviewsData.data.results);
       setCast(response.data.cast);
       setDirector(response.data.crew.filter((crew) => crew.job === "Director"));
       setWriter(
@@ -88,10 +72,12 @@ function Movie() {
     return HHMM;
   }
 
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const [zoom, setZoom] = useState(false);
+  const [showCast, setShowCast] = useState(false);
+  const handleClose = () => {
+    setZoom(false);
+    setShowCast(false);
+  };
 
   return (
     <>
@@ -103,7 +89,7 @@ function Movie() {
             src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
             alt="background-image"
           />
-          <div className="row align-items-center pt-5 px-5 movie-details position-relative mt-5">
+          <div className="row align-items-center pt-5 px-4 mx-4 movie-details position-relative mt-5 gx-5">
             <div className="col-3 poster-img-container position-relative p-0">
               <img
                 className="poster-img img-fluid rounded-3 w-100 h-100"
@@ -111,11 +97,11 @@ function Movie() {
                 alt={movie.title}
               />
               <div
-                onClick={handleShow}
+                onClick={() => setZoom(true)}
                 className="poster-expand rounded w-100 h-100"
               >
-                <span>
-                  <i class="bi bi-zoom-in"></i>Expand
+                <span className="fs-4">
+                  <i className="bi bi-zoom-in"></i> Expand
                 </span>
               </div>
             </div>
@@ -123,7 +109,7 @@ function Movie() {
               <div className="d-flex flex-column">
                 <div className="d-flex align-items-center">
                   <h1 className="text-start fs-2" style={styles.h1}>
-                    {movie.title}{" "}
+                    <strong>{movie.title}</strong>{" "}
                     <span className="year">
                       (
                       {Object.keys(movie).length > 0 &&
@@ -200,27 +186,43 @@ function Movie() {
             </div>
           </div>
         </div>
-        <div className="mt-5 px-5">
-          <h3 className="text-start mb-3 fs-2">Cast</h3>
+        <div className="cast mt-5 px-5">
+          <h3 className="text-start mb-3 fs-2">Top Billed Cast</h3>
           {cast && (
             <Carousel
               swipeable={true}
-              draggable={true}
-              autoPlaySpeed={100000000}
-              ssr={false}
+              arrows={true}
+              autoPlay={false}
               infinite={true}
-              responsive={responsive}
+              shouldResetAutoplay={false}
+              responsive={{
+                desktop: {
+                  breakpoint: { max: 3000, min: 1024 },
+                  items: 6,
+                  slidesToSlide: 1,
+                },
+                tablet: {
+                  breakpoint: { max: 1024, min: 464 },
+                  items: 3,
+                  slidesToSlide: 1,
+                },
+                mobile: {
+                  breakpoint: { max: 464, min: 0 },
+                  items: 1,
+                  slidesToSlide: 1,
+                },
+              }}
               keyBoardControl={true}
-              containerClass="carousel-container"
+              containerClass="carousel-container py-2 rounded"
+              itemClass="px-4"
               removeArrowOnDeviceType={["tablet", "mobile"]}
-              partialVisible={true}
             >
               {cast.map(
                 (cast, i) =>
                   i <= 10 && (
                     <div
                       key={cast.name}
-                      className="cast-card w-100 text-white pb-4 rounded-3 border border-danger shadow"
+                      className="cast-card w-100 text-white pb-4 rounded-3 shadow"
                       style={styles.cardBody}
                     >
                       {cast.profile_path ? (
@@ -247,10 +249,20 @@ function Movie() {
               )}
             </Carousel>
           )}
+          <div className="view-more-cast text-end mt-2 pe-1">
+            <p onClick={() => setShowCast(true)} className="fs-4">
+              View full cast ↣
+            </p>
+          </div>
+        </div>
+        <hr className="mt-5 mx-5 bg-secondary" />
+        <div className="reviews-container mt-5 px-5">
+          <h3 className="text-start mb-3 fs-2">Reviews</h3>
+          <div className="border"></div>
         </div>
       </div>
 
-      <Modal show={show} onHide={handleClose} centered>
+      <Modal show={zoom} onHide={handleClose} centered>
         <Modal.Header
           className="bg-dark text-white"
           closeButton
@@ -270,13 +282,36 @@ function Movie() {
         </Modal.Header>
         <Modal.Body className="bg-dark p-0">
           <img
-            onClick={handleShow}
+            onClick={() => setZoom(true)}
             className="img-fluid rounded-3"
             src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
             alt={movie.title}
           />
         </Modal.Body>
       </Modal>
+
+      <Modal show={showCast} onHide={handleClose} centered>
+        <Modal.Header
+          className="bg-dark text-white"
+          closeButton
+          closeVariant="white"
+        >
+          <Modal.Title>
+            <h1 className="text-start fs-2" style={styles.h1}>
+              Cast
+            </h1>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="bg-dark p-0">
+          <img
+            onClick={() => setZoom(true)}
+            className="img-fluid rounded-3"
+            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+            alt={movie.title}
+          />
+        </Modal.Body>
+      </Modal>
+
       <ScrollToTopBtn />
     </>
   );
